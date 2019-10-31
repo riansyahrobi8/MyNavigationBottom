986d0ca1ba61fe7e00bec45cb2a5608a473b1db9
Robi Dahariansyah <robidahariansyah8@gmail.com>
2019-10-31 12:48:24 +0700

    coba menerapkan sqlitedb

--- app/src/main/java/com/gmail/robidahariansyah8/mynavigationbottom/fragments/FavoriteFragment.java
+++ app/src/main/java/com/gmail/robidahariansyah8/mynavigationbottom/fragments/FavoriteFragment.java
@@ -2,12 +2,11 @@ package com.gmail.robidahariansyah8.mynavigationbottom.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.gmail.robidahariansyah8.mynavigationbottom.R;
 
@@ -15,7 +14,6 @@ import com.gmail.robidahariansyah8.mynavigationbottom.R;
  * A simple {@link Fragment} subclass.
  */
 public class FavoriteFragment extends Fragment {
-
 
     public FavoriteFragment() {
         // Required empty public constructor
@@ -28,5 +26,4 @@ public class FavoriteFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite, container, false);
     }
-
 }
